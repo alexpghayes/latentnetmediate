7cb1a73ca82ab8d6c9125364c1f463afec75cf4e
Alex Hayes <alexpghayes@gmail.com>
2022-06-08 19:07:18 -0500

    Minor cleanup

--- R/model-plot.R
+++ R/model-plot.R
@@ -51,6 +51,7 @@ threshold <- function(X, threshold = 1e-10) {
 #' Title
 #'
 #' @param model TODO
+#' @param A TODO
 #'
 #' @return TODO
 #' @export
@@ -92,10 +93,76 @@ plot_expected_a_post_trt <- function(model) {
 #' @rdname plot_expected_a_pre_trt
 #' @export
 plot_expected_a_pre_post_diff <- function(model) {
+
+  .NotYetImplemented()
+
   stopifnot(inherits(model, "mediator"))
 
   diff <- expected_a_post_trt(model) - expected_a_pre_trt(model)
   diff_nice <- methods::as(methods::as(threshold(diff), "TsparseMatrix"), "dgTMatrix")
+
+  plot_intervention_on_x <- function(model, post_multiply = FALSE) {
+
+    # plot treatment assignments
+
+    if (post_multiply) {
+      X <- model$X
+      C <- model$C
+    } else {
+      X <- model$UX
+      C <- model$UC
+    }
+
+    X_df <- as.matrix(X) |>
+      as_tibble() |>
+      mutate(
+        type = "post",
+        id = 1:nrow(X),
+        trt = ifelse(model$trt == 1, "treatment", "control"),
+        moved = Matrix::rowSums(abs(X - C)) > 1e-10
+      )
+
+    C_df <- as.matrix(C) |>
+      as_tibble() |>
+      mutate(
+        type = "pre",
+        id = 1:nrow(C),
+        trt = ifelse(model$trt == 1, "treatment", "control"),
+        moved = Matrix::rowSums(abs(X - C)) > 1e-10
+      )
+
+    colnames(X_df) <- colnames(C_df)
+
+    bound <- dplyr::bind_rows(X_df, C_df) |>
+      mutate(
+        color = dplyr::case_when(
+          trt == "treatment" & !moved ~ "treated-stayed",
+          trt == "treatment" & type == "pre" & moved ~ "start",
+          trt == "treatment" & type == "post" & moved ~ "finish",
+          trt == "control" ~ "control-stayed"
+        )
+      )
+
+    columns <- setdiff(colnames(bound), c("trt", "id", "type", "color", "moved"))
+
+    p <- ggpairs(
+      bound,
+      mapping = aes(alpha = 0.1),
+      columns = columns,
+      lower = list(
+        continuous = plot_shift
+      ),
+      upper = list(
+        continuous = plot_shift
+      ),
+      diag = list(
+        mapping = aes(color = NULL)
+      )
+    ) +
+      theme_classic()
+
+    p
+  }
 
   Matrix::summary(diff_nice) |>
     ggplot2::ggplot(ggplot2::aes(x = j, y = -i, fill = x)) +
@@ -106,6 +173,23 @@ plot_expected_a_pre_post_diff <- function(model) {
     theme(
       legend.position = "none"
     )
+}
+
+plot_shift <- function(data, mapping) {
+  color_map <- c(
+    "treated-stayed" = "blue1",
+    "start" = "darkgray",
+    "finish" = "firebrick2",
+    "stayed" = "darkgray"
+  )
+
+  ggplot(data = data, mapping = mapping) +
+    geom_point(aes(color = color)) +
+    geom_line(aes(group = id), color = "firebrick2") +
+    geom_vline(xintercept = 0) +
+    geom_hline(yintercept = 0) +
+    scale_color_manual(values = color_map) +
+    expand_limits(x = 0, y = 0)
 }
 
 #' @rdname plot_expected_a_pre_trt
@@ -127,92 +211,12 @@ plot_x_pre_post_diff <- function(model) {
     )
 }
 
-#'
-#' plot_shift <- function(data, mapping) {
-#'   color_map <- c(
-#'     "treated-stayed" = "blue1",
-#'     "start" = "darkgray",
-#'     "finish" = "firebrick2",
-#'     "stayed" = "darkgray"
-#'   )
-#'
-#'   ggplot(data = data, mapping = mapping) +
-#'     geom_point(aes(color = color)) +
-#'     geom_line(aes(group = id), color = "firebrick2") +
-#'     geom_vline(xintercept = 0) +
-#'     geom_hline(yintercept = 0) +
-#'     scale_color_manual(values = color_map) +
-#'     expand_limits(x = 0, y = 0)
-#' }
-#'
-#' #' TODO
-#' #'
-#' #' @param model TODO
-#' #'
-#' #' @param post_multiply TODO
-#' #'
-#' #' @return TODO
-#' #'
-#' #' @import ggplot2 GGally
-#' plot_intervention_on_x <- function(model, post_multiply = FALSE) {
-#'
-#'   # plot treatment assignments
-#'
-#'   if (post_multiply) {
-#'     X <- model$X
-#'     C <- model$C
-#'   } else {
-#'     X <- model$UX
-#'     C <- model$UC
-#'   }
-#'
-#'   X_df <- as.matrix(X) |>
-#'     as_tibble() |>
-#'     mutate(
-#'       type = "post",
-#'       id = 1:nrow(X),
-#'       trt = ifelse(model$trt == 1, "treatment", "control"),
-#'       moved = Matrix::rowSums(abs(X - C)) > 1e-10
-#'     )
-#'
-#'   C_df <- as.matrix(C) |>
-#'     as_tibble() |>
-#'     mutate(
-#'       type = "pre",
-#'       id = 1:nrow(C),
-#'       trt = ifelse(model$trt == 1, "treatment", "control"),
-#'       moved = Matrix::rowSums(abs(X - C)) > 1e-10
-#'     )
-#'
-#'   colnames(X_df) <- colnames(C_df)
-#'
-#'   bound <- dplyr::bind_rows(X_df, C_df) |>
-#'     mutate(
-#'       color = dplyr::case_when(
-#'         trt == "treatment" & !moved ~ "treated-stayed",
-#'         trt == "treatment" & type == "pre" & moved ~ "start",
-#'         trt == "treatment" & type == "post" & moved ~ "finish",
-#'         trt == "control" ~ "control-stayed"
-#'       )
-#'     )
-#'
-#'   columns <- setdiff(colnames(bound), c("trt", "id", "type", "color", "moved"))
-#'
-#'   p <- ggpairs(
-#'     bound,
-#'     mapping = aes(alpha = 0.1),
-#'     columns = columns,
-#'     lower = list(
-#'       continuous = plot_shift
-#'     ),
-#'     upper = list(
-#'       continuous = plot_shift
-#'     ),
-#'     diag = list(
-#'       mapping = aes(color = NULL)
-#'     )
-#'   ) +
-#'     theme_classic()
-#'
-#'   p
-#' }
+#' @rdname plot_expected_a_pre_trt
+#' @export
+plot_sparse_adjacency_matrix <- function(A) {
+  sign(as(A, "dgCMatrix")) %>%
+    summary(A, uniqT = FALSE) %>%
+    ggplot(aes(j, -i, fill = x)) +
+    geom_tile(fill = "black") +
+    theme_void()
+}
